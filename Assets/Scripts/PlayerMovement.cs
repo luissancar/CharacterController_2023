c0f9f6006ea6634e3091f0a9e1c7f4b86425980b
luissancar <luissancar@yahoo.es>
2023-11-26 11:16:16 +0100

    dos

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,83 +2,169 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Windows.Speech;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public CharacterController player;
+    private CharacterController player;
 
+    //Movimientos
     private float horizontalMove;
     private float verticalMove;
-    public float playerSpeed=10;
+    public float playerSpeed = 4;
     private Vector3 playerInput;
-    
-    //Gravity
-    public float gravity = 9.8f;
-    public float fallVelocity;
 
-
-    //Camara
+    //Movimiento respecto a cámara
     public Camera mainCamera;
     private Vector3 camForward;
     private Vector3 camRight;
+    // Movimiento jugador
     private Vector3 movePlayer;
 
-    private void Start()
+
+    //Gravedad
+    private float gravity = 9.8f;
+    public float fallVelocity = 1;
+
+
+    // Caida pendiente
+    public bool isOnSlope = false;
+    private Vector3 hitNormal; // la normal de nuestro jugador
+    public float slideVelocity = 7f;
+    public float slopeForceDown = -4;
+
+
+    // Salto
+    public float jumpForce;
+
+    // Animaciones
+  //  public Animator playerAnimatorController;
+
+
+    private bool yaEnPlataforma=false;
+    void Start()
     {
-        player=GetComponent<CharacterController>();
+        player = GetComponent<CharacterController>();
+
+    //    playerAnimatorController = GetComponent<Animator>();
+        
     }
-    private void Update()
+
+    // Update is called once per frame
+    void Update()
     {
         MovePlayer();
     }
 
     private void MovePlayer()
     {
-        //Movimiento
-        horizontalMove=Input.GetAxis("Horizontal");
-        verticalMove= Input.GetAxis("Vertical");
-        
-        playerInput =new Vector3(horizontalMove, 0,verticalMove);
-        playerInput=Vector3.ClampMagnitude(playerInput, 1);  /// en diagonal misma velocidad
-        //player.Move(playerInput* Time.deltaTime);
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        playerInput = new Vector3(horizontalMove,0,verticalMove);
+        playerInput =Vector3.ClampMagnitude(playerInput, 1);
+        // player.Move(playerInput*playerSpeed*Time.deltaTime);
 
 
-        //Camara
-        CamDirection();
+     //   playerAnimatorController.SetFloat("PlayerWalkVelocity", 
+       //     playerInput.magnitude * playerSpeed);
+
+        //Movimiento respecto a cámara
+        camDirection();
         movePlayer = playerInput.x * camRight + playerInput.z * camForward;
-        movePlayer = movePlayer * playerSpeed;  // aislamos la velocidad horizontal frontal de la gravedad
+        movePlayer = movePlayer * playerSpeed;
         player.transform.LookAt(player.transform.position + movePlayer);
 
+        // Gravedad
         SetGravity();
-        
+        Saltar();
+
         player.Move(movePlayer * Time.deltaTime);
-
     }
 
-    private void CamDirection()
+    private void Saltar()
     {
-        camForward=mainCamera.transform.forward;
-        camRight=mainCamera.transform.right;
-        camForward.y = 0;
-        camRight.y = 0;
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-    }
+       if (Input.GetButtonDown("Jump") && player.isGrounded)
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
 
+    //        playerAnimatorController.SetTrigger("PlayerJump");
+        }
+    }
 
     private void SetGravity()
     {
         if (player.isGrounded)
         {
-            fallVelocity = -gravity * Time.deltaTime ;
-            movePlayer.y = -fallVelocity;
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
         }
         else
         {
-            fallVelocity -= gravity * Time.deltaTime ;  //acelera
-            movePlayer.y = -fallVelocity;
+            fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+
+     //       playerAnimatorController.SetFloat("PlayerVerticalVelocity", player.velocity.y);
+//
         }
-       
+
+   //     playerAnimatorController.SetBool("IsGrounded",player.isGrounded);
+
+  //      SlideDown();
     }
+
+    private void SlideDown()
+    {
+        isOnSlope = Vector3.Angle(Vector3.up, hitNormal) >=
+            player.slopeLimit;
+        if (isOnSlope)
+        {
+            movePlayer.x += ((1f - hitNormal.y) * hitNormal.x) *
+                slideVelocity;
+            movePlayer.z += ((1f - hitNormal.y) * hitNormal.z) *
+                slideVelocity;
+            movePlayer.y += slopeForceDown;
+
+        }
+    }
+
+    private void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward= camForward.normalized;
+        camRight = camRight.normalized;
+    }
+
+/*
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        hitNormal = hit.normal;
+
+        var scriptMover2 = FindObjectOfType<Mover2>();
+        Debug.Log(hit.gameObject.tag);
+        // Comprueba si la colisión es con un objeto con un tag específico
+        if (hit.gameObject.tag == "Plataforma" && !yaEnPlataforma)
+        {
+            scriptMover2.controlador = player;
+            scriptMover2.enPlataforma = true;
+            // Haz algo cuando colisiona con el objeto
+            Debug.Log("Colisión detectada con MiObjetoTag");
+            yaEnPlataforma= true;
+        }
+        else
+        {
+            if (hit.gameObject.tag != "Plataforma")
+            {
+                scriptMover2.enPlataforma = false;
+                yaEnPlataforma = false;
+            }
+            
+        }
+    }
+*/
+   
 }
